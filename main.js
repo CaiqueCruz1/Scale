8736e79a4e7877528fe601b60280bc94316c88d0
caique <silvacruz.caique10@gmail.com>
2022-05-31 17:29:45 -0300

    Fazer Header Desktop

--- main.js
+++ main.js
@@ -7,3 +7,10 @@ function showNavOnScroll() {
     }
 }
 
+function openMenu() {
+    document.body.classList.add('menu-expanded')
+}
+
+function closeMenu() {
+    document.body.classList.remove('menu-expanded')
+}
